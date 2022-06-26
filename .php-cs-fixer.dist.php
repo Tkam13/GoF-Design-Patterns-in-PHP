6c4f4476e3a1c88fece758dadb25a7487bc7b7d8
Kandam <h1ro.kmd13@gmail.com>
2022-06-26 16:35:22 +0000

    php-cs-fixerの設定追加

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * This document has been generated with
+ * https://mlocati.github.io/php-cs-fixer-configurator/#version:3.8.0|configurator
+ * you can change this configuration by importing this file.
+ */
+$config = new PhpCsFixer\Config();
+
+return $config
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PHP81Migration' => true,
+        '@PHP80Migration:risky' => true,
+        '@PHPUnit84Migration:risky' => true,
+        '@PhpCsFixer' => true,
+        '@PhpCsFixer:risky' => true,
+        '@PSR12' => true,
+        '@PSR12:risky' => true,
+    ])
+    ->setFinder(
+        PhpCsFixer\Finder::create()
+    ->exclude('vendor')
+    ->in(__DIR__)
+    )
+;
